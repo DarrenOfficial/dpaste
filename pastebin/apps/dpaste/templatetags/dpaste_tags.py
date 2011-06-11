e38a7b89ababaa1a7be30eedcd905ccf71f19ceb
Martin Mahner <martin@mahner.org>
2011-06-11 14:19:16 +0200

    Alternative snippet highlighting using a templatetag which might work better than having it on-save.

--- pastebin/apps/dpaste/templatetags/dpaste_tags.py
+++ pastebin/apps/dpaste/templatetags/dpaste_tags.py
@@ -1,7 +1,17 @@
 from django.template import Library
+from django.utils.safestring import mark_safe
+
+from pastebin.apps.dpaste.highlight import pygmentize
 
 register = Library()
 
 @register.filter
 def in_list(value,arg):
     return value in arg
+
+@register.filter
+def highlight(snippet):
+    h = pygmentize(snippet.content, snippet.lexer)
+    if not h:
+        return snippet.content.splitlines()
+    return h.splitlines()
