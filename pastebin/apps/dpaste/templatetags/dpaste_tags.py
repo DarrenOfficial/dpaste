b5800e0091cd5ad0a8e2f63a262b4ee65320f7f7
Martin Mahner <martin@mahner.org>
2012-04-14 20:35:58 +0200

    Better snippet list.

--- pastebin/apps/dpaste/templatetags/dpaste_tags.py
+++ pastebin/apps/dpaste/templatetags/dpaste_tags.py
@@ -10,8 +10,12 @@ def in_list(value,arg):
     return value in arg
 
 @register.filter
-def highlight(snippet):
+def highlight(snippet, maxlines=None):
     h = pygmentize(snippet.content, snippet.lexer)
     if not h:
-        return snippet.content.splitlines()
-    return h.splitlines()
+        s = snippet.content.splitlines()
+    s = h.splitlines()
+
+    if maxlines:
+        return s[:maxlines]
+    return s
