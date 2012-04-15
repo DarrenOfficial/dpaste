bd9ad00aeefe82400c2a859d5f74682134c8054d
Martin Mahner <martin@mahner.org>
2012-04-15 14:30:25 +0000

    Raw id field for performance

--- pastebin/apps/dpaste/admin.py
+++ pastebin/apps/dpaste/admin.py
@@ -5,5 +5,6 @@ class SnippetAdmin(admin.ModelAdmin):
     list_display = ('published', 'expires', 'lexer', 'get_absolute_url')
     date_hierarchy = 'published'
     list_filter = ('published',)
+    raw_id_fields = ('parent',)
 
 admin.site.register(Snippet, SnippetAdmin)
