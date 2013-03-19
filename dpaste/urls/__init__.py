4183c58cdd24158aaedda37cbc3ca2f0a11025f5
Martin Mahner <martin@mahner.org>
2013-03-19 14:46:06 +0100

    Upgraded to Django 1.5. Fixed urls and paths

--- dpaste/urls/__init__.py
+++ dpaste/urls/__init__.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import url, patterns, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^about/$', 'django.views.generic.simple.direct_to_template', {'template': 'about.html'}, name='about'),
+    url(r'^about/$', TemplateView.as_view(template_name='dpaste/about.html'), name='about'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('dpaste.urls.dpaste_api')),
     url(r'^', include('dpaste.urls.dpaste')),
