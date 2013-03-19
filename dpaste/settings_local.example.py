833d0988db0ccebc1dcbe741b8235afec3eebef2
Martin Mahner <martin@mahner.org>
2013-03-19 13:55:46 +0100

    Added some example settings.

---
+++ dpaste/settings_local.example.py
@@ -0,0 +1,22 @@
+from dpaste.settings import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    #('Your Name', 'name@example.com'),
+)
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dpaste',
+        'USER': 'root',
+        'PASSWORD': '',
+    }
+}
+
+SECRET_KEY = 'changeme'
+
+EMAIL_BACKEND = 'dpaste.smtp.EmailBackend'
