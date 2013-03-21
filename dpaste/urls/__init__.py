22bce7a0080c18295e8199612b1eb79f8ab551b0
Martin Mahner <martin@mahner.org>
2013-03-21 19:40:48 +0100

    Removed Django admin. It was never useful anyway.

--- dpaste/urls/__init__.py
+++ dpaste/urls/__init__.py
@@ -1,13 +1,9 @@
 from django.conf.urls.defaults import url, patterns, include
-from django.contrib import admin
 from django.views.generic import TemplateView
-
-admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^about/$', TemplateView.as_view(template_name='dpaste/about.html'), name='about'),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('dpaste.urls.dpaste_api')),
     url(r'^', include('dpaste.urls.dpaste')),
 )
