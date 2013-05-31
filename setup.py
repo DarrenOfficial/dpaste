3c871c4b90e4cb8afdfb4424d8b595dbc4823eb9
Martin Mahner <martin@mahner.org>
2013-05-31 11:48:48 +0200

    Bumped up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dpaste',
-      version='1.0',
+      version='1.1',
       packages=find_packages(),
       package_data={'dpaste': ['static/*.*', 'templates/*.*']},
       scripts=['manage.py'])
