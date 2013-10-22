815bc322b0746883700dc959e83bff180d351b7c
Marius Gedminas <marius@gedmin.as>
2013-10-22 10:34:29 +0300

    Include a trailing newline in API responses
    This fixes an annoyance where using, e.g.,
    
      git diff | curl -F 'content=<-' https://dpaste.de/api/
    
    would place the bash prompt on the same line as the response.

--- dpaste/views.py
+++ dpaste/views.py
@@ -256,8 +256,8 @@ def snippet_api(request, enclose_quotes=True):
 
     response = 'https://dpaste.de%s' % s.get_absolute_url()
     if enclose_quotes:
-        return HttpResponse('"%s"' % response)
-    return HttpResponse(response)
+        return HttpResponse('"%s"\n' % response)
+    return HttpResponse(response + '\n')
 
 
 # -----------------------------------------------------------------------------
