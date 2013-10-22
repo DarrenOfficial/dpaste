c83c908afb53a796e12e13b11d4dbfa26064410f
Marius Gedminas <marius@gedmin.as>
2013-10-22 10:28:27 +0300

    Return HTTPS URLs in API responses

--- dpaste/views.py
+++ dpaste/views.py
@@ -254,7 +254,7 @@ def snippet_api(request, enclose_quotes=True):
     )
     s.save()
 
-    response = 'http://dpaste.de%s' % s.get_absolute_url()
+    response = 'https://dpaste.de%s' % s.get_absolute_url()
     if enclose_quotes:
         return HttpResponse('"%s"' % response)
     return HttpResponse(response)
