1fce9e38f67991763c9b0e3ff240c70f274887b6
Martin Mahner <martin@mahner.org>
2013-11-23 16:08:11 +0100

    Reorganised settings. Removed Redis cache from base settings.

--- dpaste/settings/__init__.py
+++ dpaste/settings/__init__.py
@@ -115,16 +115,6 @@ DATABASES = {
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
-CACHES = {
-    "default": {
-        "BACKEND": "redis_cache.cache.RedisCache",
-        "LOCATION": "127.0.0.1:6379:1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "redis_cache.client.DefaultClient",
-        }
-    }
-}
-
 #==============================================================================
 # App specific settings
 #==============================================================================
