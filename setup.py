3ce888ad9fc8ea2a4936a6624eb862723fdc8a39
Martin Mahner <martin@mahner.org>
2013-11-24 21:06:46 +0100

    Cleaned up app and project based requirements.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='dpaste',
-      version='1.1',
-      packages=find_packages(),
-      package_data={'dpaste': ['static/*.*', 'templates/*.*']},
-      scripts=['manage.py'])
+setup(
+    name='dpaste',
+    version='1.2',
+    packages=find_packages(),
+    package_data={'dpaste': ['static/*.*', 'templates/*.*']},
+    scripts=('manage.py',),
+    install_requires=(
+        'django-mptt>=0.6.0',
+        'pygments>=1.6',
+        'requests>=2.0.0',
+    ),
+)
