9768ab9573b6ec92e7f592c0f68508c03b666ea6
Martin Mahner <martin@mahner.org>
2014-06-09 20:19:07 +0200

    Added Dev Changelog

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ long_description = u'\n\n'.join((
 
 setup(
     name='dpaste',
-    version='2.7',
+    version='2.8',
     description='dpaste is a Django based pastebin. It\'s intended to run '
                 'separately but its also possible to be installed into an '
                 'existing Django project like a regular app.',
