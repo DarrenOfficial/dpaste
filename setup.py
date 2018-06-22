7671362b52e3c16218c692a0054288142e3a217d
Martin Mahner <martin@mahner.org>
2018-06-22 12:57:45 +0200

    Bumped up alpha version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = '\n\n'.join((
 ))
 setup(
     name='dpaste',
-    version='3.0a5',
+    version='3.0a6',
     description='dpaste is a Django based pastebin. It\'s intended to run '
                 'separately but its also possible to be installed into an '
                 'existing Django project like a regular app.',
