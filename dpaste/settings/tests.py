e3d4725e52503e68f95b55fc079ce5c5fc4bce69
Martin Mahner <martin@mahner.org>
2019-12-07 12:05:53 +0100

    Fix Django 3.0 tests with CSP issues

--- dpaste/settings/tests.py
+++ dpaste/settings/tests.py
@@ -8,3 +8,9 @@ SECRET_KEY = "test-key"
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}
 }
+
+# Drop CSP middleware for Django 3.0 until it was fixed upstream
+# https://github.com/mozilla/django-csp/issues/129
+import django
+if django.get_version().startswith("3."):
+    MIDDLEWARE.remove("csp.middleware.CSPMiddleware")
