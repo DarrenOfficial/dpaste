1d5c465975139a1a34e24ca5a3140645008e86a0
Martin Mahner <martin@mahner.org>
2019-12-09 09:23:43 +0100

    Minor docs UI tweaks

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = None
 
 
 # -- Options for HTML output -------------------------------------------------
