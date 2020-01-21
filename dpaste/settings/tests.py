cb7719b59feaa432e7c14dde93320fda3cdc2a5e
Martin Mahner <martin@mahner.org>
2020-01-21 23:24:13 +0100

    Update tests.py

--- dpaste/settings/tests.py
+++ dpaste/settings/tests.py
@@ -8,8 +8,3 @@ from .base import *  # noqa
 SECRET_KEY = "test-key"
 
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
-
-# Drop CSP middleware for Django 3.0 until it was fixed upstream
-# https://github.com/mozilla/django-csp/issues/129
-if django.get_version().startswith("3."):
-    MIDDLEWARE.remove("csp.middleware.CSPMiddleware")
